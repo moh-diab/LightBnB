8af381edb7d14a87b19483acc01945de0411723f
moh-diab <mohdiab@gmail.com>
2022-01-07 09:53:40 -0500

    update getAllReservations function to connec to db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -71,9 +71,24 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+ const getAllReservations = function(guest_id, limit = 10) {
+  const queryString = `
+  SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE end_date < NOW()::DATE AND
+    reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+`;
+  const values = [guest_id, limit];
+  return pool.query(queryString, values)
+    .then((res) => res.rows
+    )
+    .catch((err) => console.error("query error", err.stack));
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
