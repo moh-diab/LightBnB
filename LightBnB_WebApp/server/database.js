642281f80b3cf02865017016fc7bcbd0b4c6f785
moh-diab <mohdiab@gmail.com>
2022-01-07 09:30:54 -0500

    update three functions (getUserWithEmail, getUserWithId, assUser) to connec to db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -17,18 +17,15 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+ const getUserWithEmail = function(email) {
+  const queryString = `SELECT * FROM users WHERE email = $1;`;
+  const values = [email];
+  return pool.query(queryString, values)
+    .then((res) => res.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,9 +33,15 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+ const getUserWithId = function(id) {
+  const queryString = `SELECT * FROM users WHERE id = $1;`;
+  const values = [id];
+  return pool.query(queryString, values)
+    .then((res) => res.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,12 +50,18 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+ const addUser =  function(user) {
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`;
+  const values = [user.name, user.email, user.password];
+
+  return pool.query(queryString, values)
+    .then((res) => res.rows[0]
+    )
+    .catch((err) => console.log(err.message));
+};
 exports.addUser = addUser;
 
 /// Reservations
